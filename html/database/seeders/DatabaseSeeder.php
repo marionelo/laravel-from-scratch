510d00f5e3ef47f2e87f57fd9cda952a9a4744d6
Mario Sanchez <m.sanchez@cyberpuerta.mx>
2023-07-03 22:03:48 -0600

    mejoras y avances del proyecto

--- html/database/seeders/DatabaseSeeder.php
+++ html/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,7 +16,55 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        User::truncate();
+        Category::truncate();
+        Post::truncate();
+
+        $user = User::factory(10)->create();
+
+        $personal = Category::create([
+            "name" => "personal",
+            "slug" => "personal"
+        ]);
+
+        $work = Category::create([
+            "name" => "work",
+            "slug" => "work"
+        ]);
+
+        $other = Category::create([
+            "name" => "other",
+            "slug" => "other"
+        ]);
+
+        Post::create([
+            "user_id" => $user[0]->id,
+            "category_id" => $personal->id,
+            "title" => "mi primer post",
+            "slug" => "mi-primer-post",
+            "excerpt" => "asdlkjhaskd askldh ...",
+            "body" => "sakjdhas kdjhask djashd kjashd kasjhd askjdh askjdh askjdh askjdh"
+        ]);
+
+        Post::create([
+            "user_id" => $user[1]->id,
+            "category_id" => $work->id,
+            "title" => "mi segundo post",
+            "slug" => "mi-segundo-post",
+            "excerpt" => "asdlkjhaskd askldh ...",
+            "body" => "sakjdhas kdjhask djashd kjashd kasjhd askjdh askjdh askjdh askjdh"
+        ]);
+
+        Post::create([
+            "user_id" => $user[1]->id,
+            "category_id" => $other->id,
+            "title" => "mi tercer post",
+            "slug" => "mi-tercer-post",
+            "excerpt" => "asdlkjhaskd askldh ...",
+            "body" => "sakjdhas kdjhask djashd kjashd kasjhd askjdh askjdh askjdh askjdh"
+        ]);
+
+
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
